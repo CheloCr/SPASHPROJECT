ea91cb11727d47014083831549c7edc22edfdefd
CheloCr <85573546+CheloCr@users.noreply.github.com>
2022-07-11 10:23:29 -0500

    login fixed contact info solved

--- middlewares/isLoggedin.js
+++ middlewares/isLoggedin.js
@@ -1,11 +1,9 @@
 
 module.exports = (req, res, next) => {
-  // if an already logged in user tries to access the login page it
-  // redirects the user to the home page
-  if (req.session.currentUser) {
-    return res.redirect(`user/profile/${user.id}`);
+  // checks if the user is logged in when trying to access a specific page
+  if (!req.session.currentUser) {
+    return res.redirect("/auth/login");
   }
+  req.user = req.session.user;
   next();
 };
-
-  
